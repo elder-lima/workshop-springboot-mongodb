f9be5acb76e9f361fecc3d44f49196d8b322652b
Elder Lima <eldersantoss1981@gmail.com>
2025-12-06 18:13:45 -0300

    Obtendo um post por id

---
+++ src/main/java/com/elder/workshopmongo/services/PostService.java
@@ -0,0 +1,19 @@
+package com.elder.workshopmongo.services;
+
+import com.elder.workshopmongo.domain.Post;
+import com.elder.workshopmongo.repository.PostRepository;
+import com.elder.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Post não encontrado."));
+    }
+
+}
